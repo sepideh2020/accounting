f3a81041937c1a13588d868111245366dab0a6f2
sepideh2020 <sepideniktab2020@gmail.com>
2020-11-16 15:13:21 +0330

    some bug solved

--- accountM.py
+++ accountM.py
@@ -1,35 +1,65 @@
+import csv
+import os.path
+
+
 class Account:
     income_type = ["Food", "clothing", "housing"]
     cost_type = ["Salary", "profit_of_capital", "inheritance"]
 
-    def __init__(self, account_number, initial_amount, bank_name, cart_number):
+    def __init__(self, bank_name, account_number, card_number):  # lozomi nadare vaghti karbar mikhad accounteshocheck
+        # kone azash mojodisho begirim ya mikhad  variz kone
+
         self.account_number = account_number
-        self.initial_amount = initial_amount
+       #self.initial_amount = initial_amount   #initial amount inha niyazi nist
         self.bank_name = bank_name
-        self.cart_number = cart_number
-        self.balance = initial_amount
+        self.card_number = card_number
+        try:
 
-        # add to file
-        # add to logg file
-        # logger.debug("account_number:  " + self.account_number + "  initial_amount :  " + initial_amount +
-        #              "  bank_name:  " + self.bank_name + "  cart_number:  " + self.cart_number)
+            with open(
+                    '/Users/Niktab/Desktop/AI/MaktabSahrif/projects/accounting/users/users_info/mohammad/user_accounts.csv',
+                    'r') as check_account_existence:
+                reader = csv.DictReader(check_account_existence)
+                for row in reader:
 
-    @classmethod
-    def new_income(cls, income):  # income name changed
-        cls.income_type.append(income)
-
-    @classmethod
-    def new_cost(cls, cost):  # cost name changed
-        cls.cost_type.append(cost)
-
-    def spend_account_balance(self, amount):
-        self.balance -= amount
-
-    def earn_income(self, amount):
-        self.balance += amount
-
-    def __str__(self):
-        return "Balance:" + str(self.balance) + "Bank_name:" + self.bank_name + "cart_number :" + self.cart_number
+                    if row['Card Number'] == str(self.card_number) and row['Account Number'] == str(self.account_number) and row['Bank Name'] == self.bank_name:
+                        self.account_balance = row['Account Balance']
+                        # print(self.account_balance)
+                        self.initial_amount=row['Initial Amount']
+                        # print(self.initial_amount)
+                    else:
+                        print("Account does not exist")
+        except FileNotFoundError:
+            print("File does not exist")
 
 
-a = Account("50222910", "1000", "pasargad", "552")
+
+
+# add to file
+# add to logg file
+# logger.debug("account_number:  " + self.account_number + "  initial_amount :  " + initial_amount +
+#              "  bank_name:  " + self.bank_name + "  card_number:  " + self.card_number)
+
+
+#
+#     @classmethod
+#     def new_income(cls, income):  # income name changed
+#         cls.income_type.append(income)
+#
+#     @classmethod
+#     def new_cost(cls, cost):  # cost name changed
+#         cls.cost_type.append(cost)
+#
+#     def spend_account_balance(self, amount):
+#         self.account_balance -= amount
+#
+#     def earn_income(self, amount):
+#         self.account_balance += amount
+#
+#     def __str__(self):
+#         return "Balance:" + str(
+#             self.account_balance) + "Bank_name:" + self.bank_name + "card_number :" + self.card_number
+#
+#
+#a = Account("Melat".capitalize(),1234, 5000)
+#mohammad = Account("tejarat".capitalize(), 1234, 5000)
+# print(mohammad)
--- userM.py
+++ userM.py
@@ -1,5 +1,4 @@
 import os
-from accountM import Account
 import csv
 import logging
 
@@ -7,14 +6,14 @@ import logging
 class User:
     def __init__(self):
         self.bank_name = None
-        self.cart_number = 0
+        self.card_number = 0
         self.account_number = 0
         self.initial_amount = 0
         self.account_balance = 0
         with open(
-                '/Users/Niktab/Desktop/AI/MaktabSahrif/projects/accounting/users/users_info\mohammad/user_accounts.csv',
+                '/Users/Niktab/Desktop/AI/MaktabSahrif/projects/accounting/users/users_info/mohammad/user_accounts.csv',
                 'w', newline='') as user_accounts:
-            fieldnames = ['Bank Name', 'Cart Number ', 'Account Number', 'Initial Amount', 'Account Balance']
+            fieldnames = ['Bank Name', 'Card Number', 'Account Number', 'Initial Amount', 'Account Balance']
             headers = csv.DictWriter(user_accounts, fieldnames=fieldnames)
             headers.writeheader()
 
@@ -22,14 +21,14 @@ class User:
 
         # self.accounts = []
 
-    def new_account(self, bank_name, account_number, cart_number, initial_amount):
+    def new_account(self, bank_name, account_number, card_number, initial_amount):
         """BY CALLING THIS FUNCTION USER CAN MAKE A NEW ACCOUNT AND ADD IT TO THE USERS ACCOUNT.CSV IF DOES NOT EXITS
         ,WHEN THE ACCOUNT IS ADDED A TRANSACTION.CSV FILE IS MADE FOR ADDING TRANSACTIONS WHICH ARE RELATED TO THAT ACCOUNT
         """
         self.bank_name = bank_name.capitalize()
         self.account_number = account_number
         self.initial_amount = initial_amount
-        self.cart_number = cart_number
+        self.card_number = card_number
         self.account_balance = initial_amount
         # os.path.join(path, bank_name)
         try:
@@ -43,42 +42,47 @@ class User:
         with open(
                 '/Users/Niktab/Desktop/AI/MaktabSahrif/projects/accounting/users/users_info\mohammad/user_accounts.csv',
                 'w', newline='') as user_accounts:
-            fieldnames = ['Bank Name', 'Cart Number ', 'Account Number', 'Initial Amount', 'Account Balance']
+            fieldnames = ['Bank Name', 'Card Number', 'Account Number', 'Initial Amount', 'Account Balance']
             headers = csv.DictWriter(user_accounts, fieldnames=fieldnames)
             headers.writeheader()  # users_account.csv file is made is user_info directory
             headers.writerow(
-                {'Bank Name': self.bank_name, 'Cart Number ': self.cart_number, 'Account Number': self.account_number,
+                {'Bank Name': self.bank_name, 'Card Number': self.card_number, 'Account Number': self.account_number,
                  'Initial Amount': self.initial_amount, 'Account Balance': self.account_balance})
 
         with open(os.path.join(new_dir, 'transactions.csv'), 'w', newline='') as transactions:
             fieldnames = ['Withdraw or Deposit amount', 'Type', 'Date']
             headers = csv.DictWriter(transactions, fieldnames=fieldnames)
             headers.writeheader()  # each bank account will have a transaction file
+        # logging.info('This is an info message')
 
-    # self.accounts.append(Account(account_number, initial_amount, bank_name, cart_number))
-
-    def spend(self, unique, value, category, ):
-        # unique == account_number or cart_number
-        for account in self.accounts:
-            if account.account_number == unique or account.cart_number == unique:
-                account.spend_account_balance(value)
-                # file add (account_number , cart_number ,value,category)
-                # log : successfully translate
+    # self.accounts.append(Account(account_number, initial_amount, bank_name, card_number))
+    #
+    # def spend(self, unique, value, category, ):
+    #     # unique == account_number or card_number
+    #     for account in self.accounts:
+    #         if account.account_number == unique or account.card_number == unique:
+    #             account.spend_account_balance(value)
+    #             # file add (account_number , card_number ,value,category)
+    #             # log : successfully translate
 
 
 # def earn(self, unique, value, category):
 #     for account in self.accounts:
-#         if account.account_number == unique or account.cart_number == unique:
+#         if account.account_number == unique or account.card_number == unique:
 #             account.earn_income(value)
-#             # file add (account_number , cart_number ,value,category)
+#             # file add (account_number , card_number ,value,category)
 #             # log : successfully translate
 #
 # @staticmethod
 # def show_account(unique):
 #     for account in User.accounts:
-#         if account.account_number == unique or account.cart_number == unique:
+#         if account.account_number == unique or account.card_number == unique:
 #             print(account)
 
-mohammad = User()
-mohammad.new_account("tejarat".capitalize(), 1234, 5000, 43321)
-print(mohammad)
+
+mohammad= User()
+mohammad.new_account("tejarat".capitalize(), 32456, 66879 ,37675)
+ali= User()
+ali.new_account("tejarat".capitalize(), 35465768, 5678890 ,34654789)
+print(ali)
+#ro ghablie minevise chheraaa??
